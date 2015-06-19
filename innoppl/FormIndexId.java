e2c2d4acd5617709464c4ceca6d09c373ec3f7fa
surendarm <surendar.info@gmail.com>
2015-06-19 12:08:10 +0530

    Commit with sample java files

---
+++ innoppl/FormIndexId.java
@@ -0,0 +1,29 @@
+package com.innoppl;
+
+public class FormIndexId {
+
+	private int submissionID;
+	private String timer;
+	private String status;
+	public int getSubmissionID() {
+		return submissionID;
+	}
+	public void setSubmissionID(int submissionID) {
+		this.submissionID = submissionID;
+	}
+	public String getTimer() {
+		return timer;
+	}
+	public void setTimer(String timer) {
+		this.timer = timer;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	
+
+}
